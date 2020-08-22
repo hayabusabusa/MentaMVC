2db2691d061844f4d6f355f87136ef1322b449b7
hayabusabusa <hayabusabusaSY@gmail.com>
2020-08-22 14:51:19 +0900

    Add table view on items view controller

---
+++ MentaMVC/ViewController/Items/Cell/ItemsViewControllerCell.swift
@@ -0,0 +1,35 @@
+//
+//  ItemsViewControllerCell.swift
+//  MentaMVC
+//
+//  Created by Shunya Yamada on 2020/08/22.
+//  Copyright © 2020 Shunya Yamada. All rights reserved.
+//
+
+import UIKit
+
+class ItemsViewControllerCell: UITableViewCell {
+    
+    // MARK: IBOutlet
+    
+    // MARK: Properties
+    
+    static let reuseIdentifier: String = "ItemsViewControllerCell"
+    static let nib: UINib = UINib(nibName: "ItemsViewControllerCell", bundle: nil)
+    
+    // MARK: Lifecycle
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+    }
+    
+    // MARK: Configurations
+    
+    func configureCell() {
+        
+    }
+}
--- MentaMVC/ViewController/Items/ItemsViewController.swift
+++ MentaMVC/ViewController/Items/ItemsViewController.swift
@@ -12,11 +12,54 @@ final class ItemsViewController: UIViewController {
     
     // MARK: IBOutlet
     
+    @IBOutlet private weak var tableView: UITableView!
+    
     // MARK: Properties
     
     // MARK: Lifecycle
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        configureNavigation()
+        configureTableView()
+    }
+}
+
+// MARK: - Configurations
+
+extension ItemsViewController {
+    
+    private func configureNavigation() {
+        
+    }
+    
+    private func configureTableView() {
+        tableView.delegate = self
+        tableView.dataSource = self
+        tableView.rowHeight = UITableView.automaticDimension
+        tableView.register(ItemsViewControllerCell.nib, forCellReuseIdentifier: ItemsViewControllerCell.reuseIdentifier)
+    }
+}
+
+// MARK: - TableView DataSource
+
+extension ItemsViewController: UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: ItemsViewControllerCell.reuseIdentifier, for: indexPath) as! ItemsViewControllerCell
+        return cell
+    }
+}
+
+// MARK: - TableView Delegate
+
+extension ItemsViewController: UITableViewDelegate {
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
     }
 }
