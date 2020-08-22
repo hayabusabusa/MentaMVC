071a9e37f2ace11cf8008d19c983897746bbd02f
hayabusabusa <hayabusabusaSY@gmail.com>
2020-08-22 22:08:51 +0900

    Update cell configure function

--- MentaMVC/ViewController/Items/Cell/ItemsViewControllerCell.swift
+++ MentaMVC/ViewController/Items/Cell/ItemsViewControllerCell.swift
@@ -7,15 +7,23 @@
 //
 
 import UIKit
+import Kingfisher
 
 class ItemsViewControllerCell: UITableViewCell {
     
     // MARK: IBOutlet
     
+    @IBOutlet private weak var profileImageView: UIImageView!
+    @IBOutlet private weak var titleLabel: UILabel!
+    @IBOutlet private weak var bodyLabel: UILabel!
+    @IBOutlet private weak var tagsLabel: UILabel!
+    @IBOutlet private weak var likesCountLabel: UILabel!
+    @IBOutlet private weak var commentsCountLabel: UILabel!
+    
     // MARK: Properties
     
     static let reuseIdentifier: String = "ItemsViewControllerCell"
-    static let estimatedRowHeight: CGFloat = 145
+    static let estimatedRowHeight: CGFloat = 143
     static let nib: UINib = UINib(nibName: "ItemsViewControllerCell", bundle: nil)
     
     // MARK: Lifecycle
@@ -30,7 +38,16 @@ class ItemsViewControllerCell: UITableViewCell {
     
     // MARK: Configurations
     
-    func configureCell() {
+    func configureCell(profileImageURL: String?, title: String?, body: String?, tags: String?, likesCount: Int, commentsCount: Int) {
+        titleLabel.text = title
+        bodyLabel.text = body
+        tagsLabel.text = tags
+        tagsLabel.superview?.isHidden = tags == nil
+        likesCountLabel.text = "\(likesCount)"
+        commentsCountLabel.text = "\(commentsCount)"
         
+        if let url = URL(string: profileImageURL ?? "") {
+            profileImageView.kf.setImage(with: url, placeholder: nil, options: [.transition(.fade(0.3))])
+        }
     }
 }
