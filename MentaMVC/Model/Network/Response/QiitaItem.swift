84bfea872a7972f821409536a6b0a8cf4526deaf
hayabusabusa <hayabusabusaSY@gmail.com>
2020-09-04 14:23:54 +0900

    Add API response model objects

---
+++ MentaMVC/Model/Network/Response/QiitaItem.swift
@@ -0,0 +1,29 @@
+//
+//  QiitaItem.swift
+//  MentaMVC
+//
+//  Created by 山田隼也 on 2020/09/04.
+//  Copyright © 2020 Shunya Yamada. All rights reserved.
+//
+
+import Foundation
+
+struct QiitaItem: Decodable {
+    let title: String?
+    let body: String?
+    let url: String?
+    let likesCount: Int
+    let commentsCount: Int
+    let user: QiitaUser
+    let tags: [QiitaItemTag]
+    
+    private enum CodingKeys: String, CodingKey {
+        case title
+        case body
+        case url
+        case likesCount = "likes_count"
+        case commentsCount = "comments_count"
+        case user
+        case tags
+    }
+}
---
+++ MentaMVC/Model/Network/Response/QiitaItemTag.swift
@@ -0,0 +1,13 @@
+//
+//  QiitaItemTag.swift
+//  MentaMVC
+//
+//  Created by 山田隼也 on 2020/09/04.
+//  Copyright © 2020 Shunya Yamada. All rights reserved.
+//
+
+import Foundation
+
+struct QiitaItemTag: Decodable {
+    let name: String
+}
