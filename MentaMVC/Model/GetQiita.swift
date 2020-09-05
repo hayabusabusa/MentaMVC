2d2ae9dbf1a44661cdf7b1d7ab9c4d458543c1d0
Kazufumi Yoshimoto <kazufumi2468australia@gmail.com>
2020-09-05 16:48:58 +0900

    API通信を行うModelの作成

---
+++ MentaMVC/Model/GetQiita.swift
@@ -0,0 +1,41 @@
+//
+//  GetQiita.swift
+//  MentaMVC
+//
+//  Created by 吉本和史 on 2020/09/05.
+//  Copyright © 2020 Shunya Yamada. All rights reserved.
+//
+
+import Foundation
+import Alamofire
+
+final class GetQiita {
+    private var isViewLoaded: Bool = false
+
+    func onViewDidLoad() {
+        // ViewDidLoad でする処理
+        let text = "https://qiita.com/api/v2/items?page=1&per_page=20"
+        let url = text.addingPercentEncoding(withAllowedCharacters: .urlQueryAllowed)!
+        AF.request(url, method: .get, parameters: nil, encoding: JSONEncoding.default).responseJSON { response in
+            switch response.result {
+                case .success:
+                    // JSON からの変換
+                    do {
+                        let data = response.data
+                        let qiitaItems = try JSONDecoder().decode(QiitaItems.self, from: data!)
+                        print(qiitaItems)
+                    } catch {
+                        // デコードのエラー
+                        print("---ここでエラー---")
+                        print(error)
+                    }
+                break
+                case .failure:
+                  print("failure")
+                break
+            }
+        }
+        isViewLoaded = true
+    }
+}
+
