5239b51b54d33c638fa033337446b3c5409760e8
Kazufumi Yoshimoto <kazufumi2468australia@gmail.com>
2020-09-06 14:10:50 +0900

    モデルオブジェクトの修正

---
+++ MentaMVC/ViewController/Items/QiitaTags.swift
@@ -0,0 +1,13 @@
+//
+//  QiitaTags.swift
+//  MentaMVC
+//
+//  Created by 吉本和史 on 2020/09/06.
+//  Copyright © 2020 Shunya Yamada. All rights reserved.
+//
+
+import Foundation
+
+struct QiitaTags: Decodable {
+  let name: String
+}
